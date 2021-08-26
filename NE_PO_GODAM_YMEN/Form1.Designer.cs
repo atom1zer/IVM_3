b2434b6f40c0dbb35ca04a738940f8bf1c7c173e
atom1zer <dlsamsonov201133@gmail.com>
2021-08-26 21:40:19 +0300

    Добавьте файлы проекта.

---
+++ NE_PO_GODAM_YMEN/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace NE_PO_GODAM_YMEN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(163, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(163, 214);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(106, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "B10";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(72, 44);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(163, 130);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(145, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(163, 266);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(145, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(649, 370);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить поля";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(106, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "A2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 269);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "B2";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(163, 338);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(145, 20);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(163, 384);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(145, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "C10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(106, 387);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "C2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ NE_PO_GODAM_YMEN/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NE_PO_GODAM_YMEN
+{
+
+    public partial class Form1 : Form
+    {
+        public string result;
+        public int A10;
+        public int B10;
+        public string A2;
+        public string B2;
+        public int lenth_1;
+        public int lenth_2;
+        public string rez = "0000000000000000";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Функция перевода в 10ую
+        static int BinaryToDecimal(string binaryNumber)
+        {
+            int exponent = 0;
+            int result = 0;
+            for (var i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                if (binaryNumber[i] == '1')
+                {
+                    result += Convert.ToInt32(Math.Pow(2, exponent));
+                }
+                exponent++;
+            }
+
+            return result;
+        }
+
+        private string Add(string x, string y)
+        {
+            string z = "";
+            char p = '0';//Перенос при сложении двух разрядов
+            for (int i = y.Length - 1; i >= 0; i--)
+            {
+               // MessageBox.Show( " " + i);
+                if (y[i] == '0' && x[i] == '0' && p == '0') { z = '0' + z; p = '0'; continue; }
+                if (y[i] == '0' && x[i] == '0' && p == '1') { z = '1' + z; p = '0'; continue; }
+                if (y[i] == '1' && x[i] == '0' && p == '0') { z = '1' + z; p = '0'; continue; }
+                if (y[i] == '1' && x[i] == '0' && p == '1') { z = '0' + z; p = '1'; continue; }
+                if (y[i] == '0' && x[i] == '1' && p == '0') { z = '1' + z; p = '0'; continue; }
+                if (y[i] == '0' && x[i] == '1' && p == '1') { z = '0' + z; p = '1'; continue; }
+                if (y[i] == '1' && x[i] == '1' && p == '0') { z = '0' + z; p = '1'; continue; }
+                if (y[i] == '1' && x[i] == '1' && p == '1') { z = '1' + z; p = '1'; continue; }
+            }
+            return z;
+        }
+
+        public void Ymnojenie(string a2, string b2)
+        {
+            lenth_1 = a2.Length;
+            for (; lenth_1 < 16; lenth_1++)
+            {
+                a2 = a2.Insert(0, "0");
+            }
+          // textBox3.Text = a2;
+
+            lenth_2 = b2.Length;
+            for (; lenth_2 < 16; lenth_2++)
+            {
+                b2 = b2.Insert(0, "0");
+            }
+            //textBox4.Text = b2;
+
+
+            for (int i = b2.Length - 1; i >= 0; i--)
+            {
+                if (b2[i] == '1') rez = Convert.ToString(Add(rez, a2));
+
+                a2 = a2 + '0'; // Это сдвиг
+                a2 = a2.Substring(1);
+                // MessageBox.Show(" " + a2);
+            }
+            result = Convert.ToString(BinaryToDecimal(Convert.ToString(rez)));          
+            MessageBox.Show(" " + result);
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            A10 = Convert.ToInt32(textBox1.Text);
+            B10 = Convert.ToInt32(textBox2.Text);
+
+
+            if (A10 >= 0)
+            {
+                textBox3.Text = ("0." + A2);
+            }
+            else if (B10 >= 0)
+            {
+                textBox4.Text = ("0." + B2);
+            }
+            else if (A10 < 0)
+            {
+                textBox3.Text = ("1." + A2);
+            }
+            else if (B10 < 0)
+            {
+                textBox4.Text = ("1." + B2);
+            }
+
+            if (A10<0 && B10 < 0)
+            {
+                A10 = -A10;
+                B10 = -B10;
+
+                A2 = Convert.ToString(A10, 2);
+                // textBox3.Text = A2;
+                B2 = Convert.ToString(B10, 2);
+                // textBox4.Text = B2;
+                Ymnojenie(A2, B2);
+
+                textBox3.Text = ("1." + A2);
+                textBox4.Text = ("1." + B2);
+                textBox5.Text = Convert.ToString(result);
+                textBox6.Text = Convert.ToString("0." + rez);
+            } else if (A10>=0 && B10<0)
+            {
+                B10 = -B10;
+
+                A2 = Convert.ToString(A10, 2);
+                // textBox3.Text = A2;
+                B2 = Convert.ToString(B10, 2);
+                // textBox4.Text = B2;
+                Ymnojenie(A2,B2);
+
+                textBox3.Text = ("0." + A2);
+                textBox4.Text = ("1." + B2);
+                textBox5.Text = Convert.ToString("-" + result);
+                textBox6.Text = Convert.ToString("1." + rez);
+            }
+            else if (A10 < 0 && B10 >= 0)
+            {
+                A10 = -A10;
+
+                A2 = Convert.ToString(A10, 2);
+                // textBox3.Text = A2;
+                B2 = Convert.ToString(B10, 2);
+                // textBox4.Text = B2;
+                Ymnojenie(A2, B2);
+
+                textBox3.Text = ("1." + A2);
+                textBox4.Text = ("0." + B2);
+                textBox5.Text = Convert.ToString("-" + result);
+                textBox6.Text = Convert.ToString("1." + rez);
+            }
+            else if (A10 >= 0 && B10 >= 0)
+            {
+ 
+                A2 = Convert.ToString(A10, 2);
+                // textBox3.Text = A2;
+                B2 = Convert.ToString(B10, 2);
+                // textBox4.Text = B2;
+                Ymnojenie(A2, B2);
+
+                textBox3.Text = ("0." + A2);
+                textBox4.Text = ("0." + B2);
+                textBox5.Text = Convert.ToString(result);
+                textBox6.Text = Convert.ToString("0." + rez);
+            }
+            rez = "0000000000000000";
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            A10 = 0;
+            B10 = 0;
+            rez = "0000000000000000";
+
+        }
+    }
+}
